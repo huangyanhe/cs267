f88c9cf1b826514ec3f51b295bbc0ffef4453ee9
huangyanhe <yanhe_huang@berkeley.edu>
2018-02-08 19:17:40 -0800

    Add files via upload

--- serial.cpp
+++ serial.cpp
@@ -8,7 +8,7 @@
 //  benchmarking program
 //
 int main( int argc, char **argv )
-{
+{    
     int navg,nabsavg=0;
     double davg,dmin, absmin=1.0, absavg=0.0;
 
@@ -22,29 +22,29 @@ int main( int argc, char **argv )
         printf( "-no turns off all correctness checks and particle output\n");
         return 0;
     }
-
+    
     int n = read_int( argc, argv, "-n", 1000 );
 
     char *savename = read_string( argc, argv, "-o", NULL );
     char *sumname = read_string( argc, argv, "-s", NULL );
-
+    
     FILE *fsave = savename ? fopen( savename, "w" ) : NULL;
     FILE *fsum = sumname ? fopen ( sumname, "a" ) : NULL;
 
     particle_t *particles = (particle_t*) malloc( n * sizeof(particle_t) );
     set_size( n );
     init_particles( n, particles );
-
+    
     //
     //  simulate a number of time steps
     //
     double simulation_time = read_timer( );
-
+	
     for( int step = 0; step < NSTEPS; step++ )
     {
-        navg = 0;
+	navg = 0;
         davg = 0.0;
-        dmin = 1.0;
+	dmin = 1.0;
         //
         //  compute forces
         //
@@ -52,14 +52,14 @@ int main( int argc, char **argv )
         {
             particles[i].ax = particles[i].ay = 0;
             for (int j = 0; j < n; j++ )
-            apply_force( particles[i], particles[j],&dmin,&davg,&navg);
+				apply_force( particles[i], particles[j],&dmin,&davg,&navg);
         }
-
+ 
         //
         //  move particles
         //
-        for( int i = 0; i < n; i++ )
-            move( particles[i] );
+        for( int i = 0; i < n; i++ ) 
+            move( particles[i] );		
 
         if( find_option( argc, argv, "-no" ) == -1 )
         {
@@ -71,7 +71,7 @@ int main( int argc, char **argv )
             nabsavg++;
           }
           if (dmin < absmin) absmin = dmin;
-
+		
           //
           //  save if necessary
           //
@@ -80,13 +80,13 @@ int main( int argc, char **argv )
         }
     }
     simulation_time = read_timer( ) - simulation_time;
-
+    
     printf( "n = %d, simulation time = %g seconds", n, simulation_time);
 
     if( find_option( argc, argv, "-no" ) == -1 )
     {
       if (nabsavg) absavg /= nabsavg;
-    //
+    // 
     //  -The minimum distance absmin between 2 particles during the run of the simulation
     //  -A Correct simulation will have particles stay at greater than 0.4 (of cutoff) with typical values between .7-.8
     //  -A simulation where particles don't interact correctly will be less than 0.4 (of cutoff) with typical values between .01-.05
@@ -97,22 +97,22 @@ int main( int argc, char **argv )
     if (absmin < 0.4) printf ("\nThe minimum distance is below 0.4 meaning that some particle is not interacting");
     if (absavg < 0.8) printf ("\nThe average distance is below 0.8 meaning that most particles are not interacting");
     }
-    printf("\n");
+    printf("\n");     
 
     //
     // Printing summary data
     //
-    if( fsum)
+    if( fsum) 
         fprintf(fsum,"%d %g\n",n,simulation_time);
-
+ 
     //
     // Clearing space
     //
     if( fsum )
-        fclose( fsum );
+        fclose( fsum );    
     free( particles );
     if( fsave )
         fclose( fsave );
-
+    
     return 0;
 }

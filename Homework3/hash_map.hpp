b1a973606df0f1188e1db9a5cf60b52a9d622df4
Colin Wahl <cpwahl@cori06.nersc.gov>
2018-03-14 11:04:41 -0700

    Working on debugging.

--- Homework3/hash_map.hpp
+++ Homework3/hash_map.hpp
@@ -9,8 +9,8 @@ struct HashMap {
 
 //  // pointer to an array of pointers that point to local shared used
 //  global_ptr<int> ptr2used; 
-  vector<upcxx::global_ptr<int> > ptr2used;    
-  vector<upcxx::global_ptr<kmer_pair> > ptr2used;    
+  std::vector<upcxx::global_ptr<int> > ptr2used;    
+  std::vector<upcxx::global_ptr<kmer_pair> > ptr2used;    
   
   size_t my_size;
 
@@ -24,18 +24,21 @@ struct HashMap {
   bool find(const pkmer_t &key_kmer, kmer_pair &val_kmer);
 
   // Helper functions
-
+  
   // Write and read to a logical data slot in the table.
-  void write_slot(uint64_t slot, const kmer_pair &kmer);
-  kmer_pair read_slot(uint64_t slot);
-
+  //void write_slot(uint64_t slot, const kmer_pair &kmer);
+  void HashMap::write_slot(kmer_pair *slot, const kmer_pair &kmer);
+  //kmer_pair read_slot(uint64_t slot);
+  kmer_pair read_slot(kmer_pair *slot);
   // Request a slot or check if it's already used.
-  bool request_slot(uint64_t slot);
-  bool slot_used(uint64_t slot);
+  //bool request_slot(uint64_t slot);
+  bool HashMap::request_slot(int *slot);
+  bool HashMap::slot_used(int *slot);
+ //bool slot_used(uint64_t slot);
   //My own added functions
   int64_t HashMap::refdata();
   int64_t HashMap::refused();
-  int HashMap::initPtrs(vector<upcxx::global_ptr<int> > ptrInitused, vector<upcxx::global_ptr<kmer_pair> >ptrInitdata);
+  int HashMap::initPtrs(std::vector<upcxx::global_ptr<int> > ptrInitused, std::vector<upcxx::global_ptr<kmer_pair> >ptrInitdata);
 
 };
 
@@ -55,7 +58,7 @@ bool HashMap::insert(const kmer_pair &kmer) {
     uint64_t IndexOnLocalVector = slot- IndexRank*size;
     int *ptr2writeused = ptr2used[IndexRank] +  IndexOnLocalVector; 
     success = request_slot(ptr2writeused);
-    int *ptr2writedata = ptr2data[IndexRank] +  IndexOnLocalVector; 
+    kmer_pair *ptr2writedata = ptr2data[IndexRank] +  IndexOnLocalVector; 
     if (success) {
       write_slot(ptr2writedata, kmer);
     }
@@ -73,7 +76,7 @@ bool HashMap::find(const pkmer_t &key_kmer, kmer_pair &val_kmer) {
     uint64_t IndexOnLocalVector = slot- IndexRank*size;
     int *ptr2writeused = ptr2used[IndexRank] +  IndexOnLocalVector; 
     if (slot_used(ptr2writeused)) {
-        int *ptr2writedata = ptr2data[IndexRank] +  IndexOnLocalVector; 
+        kmer_pair *ptr2writedata = ptr2data[IndexRank] +  IndexOnLocalVector; 
         val_kmer = read_slot(ptr2writedata);
       if (val_kmer.kmer == key_kmer) {
         success = true;
@@ -98,11 +101,11 @@ bool HashMap::slot_used(int *slot) {
   return *slot != 0;
 }
 
-void HashMap::write_slot(int *slot, const kmer_pair &kmer) {
+void HashMap::write_slot(kmer_pair *slot, const kmer_pair &kmer) {
   *slot = kmer;
 }
 
-kmer_pair HashMap::read_slot(int *slot) {
+kmer_pair HashMap::read_slot(kmer_pair *slot) {
   return *slot;
 }
 //bool HashMap::request_slot(uint64_t slot) {

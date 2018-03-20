65a45655668d53d49da13cef2c8543fddeb62d52
Colin Wahl <colinpwahl@gmail.com>
2018-03-19 18:48:44 -0700

    Made changes that Phil suggested and cut it down to 2 pages. Also, made plots for hwk 3 in matlab.

---
+++ Matlab/Homework3/scalingPlots.m
@@ -0,0 +1,62 @@
+nodes = [1      2      4      8]; 
+timeHumanTotal = [22.307675     31.252265     23.411704     13.792652];
+timeHumanInsert = [16.317191     20.029628     14.587123     7.844210];
+timeLargeTotal = [6.918531     9.540548     7.101898     4.006188];
+timeLargeInsert = [4.993300     5.572123     3.817618    2.340963];
+timeTestTotal = [1.116526     1.438917     1.309812     0.776025];
+timeTestInsert = [0.789641      0.911556     0.834655     0.482836];
+
+figure(1)
+plot(nodes, timeHumanTotal,'-o')
+hold on
+plot(nodes, timeLargeTotal,'-o')
+plot(nodes, timeTestTotal,'-o')
+plot(nodes, timeHumanInsert,'-o')
+plot(nodes, timeLargeInsert,'-o')
+plot(nodes, timeTestInsert,'-o')
+xlabel('Nodes (w/32 Processors/Node)', 'Interpreter', 'LaTeX')
+ylabel('time', 'Interpreter', 'LaTeX')
+title('Node Scaling', 'Interpreter', 'LaTeX')
+axis([1 8 0 35])
+legend('Human Total', 'Large Total', 'Test Total', 'Human Insertion', 'Large Insertion', 'Test Insertion')
+set(gcf, 'color', 'w')
+
+Processors = [1     2      4      8   16    32    64]; 
+timeHumanTotalP = [nan       199.620335     124.953682     70.877791     41.494250     22.670032       19.971469];
+timeHumanInsertP = [nan      137.511313     91.382668     52.296240     29.624552     16.628471       13.383914];
+timeLargeTotalP = [72.483718     59.794885     nan    21.277969      nan        8.670716     7.514075];
+timeLargeInsertP = [35.782025    41.186960     nan    15.621722      nan        6.228951     4.828977];
+timeTestTotalP = [17.292061     9.436486     nan    3.511923      nan        1.113242     1.036404];
+timeTestInsertP = [8.902240    6.563542     nan    2.615434      nan         0.756301    0.658039];
+
+figure(2)
+plot(Processors, timeHumanTotalP,'-o')
+hold on
+plot(Processors, timeLargeTotalP,'-o')
+plot(Processors, timeTestTotalP,'-o')
+plot(Processors, timeHumanInsertP,'-o')
+plot(Processors, timeLargeInsertP,'-o')
+plot(Processors, timeTestInsertP,'-o')
+xlabel('Processors', 'Interpreter', 'LaTeX')
+ylabel('time', 'Interpreter', 'LaTeX')
+title('Processor Scaling (N=1)', 'Interpreter', 'LaTeX')
+axis([1 64 0 200])
+legend('Human Total', 'Large Total', 'Test Total', 'Human Insertion', 'Large Insertion', 'Test Insertion')
+set(gcf, 'color', 'w')
+
+
+
+figure(3)
+semilogy(Processors, timeHumanTotalP,'-o')
+hold on
+semilogy(Processors, timeLargeTotalP,'-o')
+semilogy(Processors, timeTestTotalP,'-o')
+semilogy(Processors, timeHumanInsertP,'-o')
+semilogy(Processors, timeLargeInsertP,'-o')
+semilogy(Processors, timeTestInsertP,'-o')
+xlabel('Processors', 'Interpreter', 'LaTeX')
+ylabel('$\log$(time)', 'Interpreter', 'LaTeX')
+title('Processor Scaling (N=1)', 'Interpreter', 'LaTeX')
+axis([1 64 0 200])
+legend('Human Total', 'Large Total', 'Test Total', 'Human Insertion', 'Large Insertion', 'Test Insertion')
+set(gcf, 'color', 'w')

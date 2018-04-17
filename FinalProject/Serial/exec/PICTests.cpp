985f8b4861d82edab3addcb555dc4c9b5f194cd2
Colin Wahl <colinpwahl@gmail.com>
2018-04-16 21:39:12 -0700

    Checked that input is correct. Now debugging 1 time step.

--- FinalProject/Serial/exec/PICTests.cpp
+++ FinalProject/Serial/exec/PICTests.cpp
@@ -5,10 +5,15 @@
 #include "RectMDArray.H"
 #include "ParticleSet.H"
 #include "ParticleVelocities.H"
-#include "CutoffKernel.H"
 #include "WriteRectMDArray.H" 
 #include "VisitWriter.H"
 #include "RK4.H"
+auto removeParticle = [](Particle p) -> bool
+{
+  double minStrength = pow(10.0, -9);
+  bool removeIfFalse =  (p.strength < minStrength);
+  return removeIfFalse;
+};
 void outField(ParticleSet& p, int a_coarsenFactor)
 {
   int coarsenFactor = a_coarsenFactor;
@@ -50,38 +55,44 @@ int main(int argc, char* argv[])
   unsigned int N;
   cout << "input test = 1 (Linear Landau Damping), 2, other" << endl;
   int test;
+  cin >> test;
   cout << "input log_2(number of grid points)" << endl; 
   cin >> M;
-  cin >> test;
   cout << "input order of interpolating function(lowest = 2)" << endl;
   int order;
   cin >> order;
   cout << "input smoothness of interpolating function(lowest = 0)" << endl;
   int smoothness;
   cin >> smoothness;  
-  cout << "input particle refinement factor" << endl;
-  unsigned int cfactor;
-  cin >> cfactor;
+  //cout << "input particle refinement factor" << endl;
+  //unsigned int cfactor;
+  //cin >> cfactor;
   cout << "enter stopping time" << endl;
   double timeStop;
   cin >> timeStop;
-  
+
+  //N = 2*Grid_Size in Matlab
   N = Power(2,M);
+  double alpha = 0.01;
+  double vmax = 10.0;
+  double L = 4*M_PI;
+  int cfactor = 2;
   double h = 1./N;
   double hp = h/cfactor; //pow(h,4./3.);
   int Np = 1./hp;
   hp = 1./Np;
+  //cout<<"hp = "<<hp<<endl;
   double delta = h;
   int pcfactor = 4/cfactor;
-  if (pcfactor < 1 ) pcfactor = 1;
-  cout << "number of particles per cell = " << h*h/hp/hp << endl;
+  //if (pcfactor < 1 ) pcfactor = 1;
+  //cout << "number of particles per cell = " << h*h/hp/hp << endl;
 
   int intlowCorner[DIM];
   int inthighCorner[DIM];
   for (int j = 0; j<DIM; j++)
     {
       intlowCorner[j] = 0;
-      inthighCorner[j] = Np-1;
+      inthighCorner[j] = N-1;
     }
   array<double, DIM> lowCorn;
   for (int j =0; j<DIM; j++)
@@ -92,51 +103,72 @@ int main(int argc, char* argv[])
   Point lowCorner(intlowCorner);
   Point highCorner(inthighCorner);
   DBox domain(lowCorner, highCorner);
-  
+  //domain.print();
   ParticleSet p(domain, hp, lowCorn, M, order, smoothness );
 
   //Assumes equally spaced grids in x and v.
   //Changing this is pretty easy.
-  int intPhaselowCorner[2*DIM];
-  int intPhasehighCorner[2*DIM];
-  for (int j = 0; j<2*DIM; j++)
+  int intPhaseXlowCorner[DIM];
+  int intPhaseXhighCorner[DIM];
+  int intPhaseVlowCorner[DIM];
+  int intPhaseVhighCorner[DIM];
+  for (int j = 0; j<DIM; j++)
     {
-      intPhaselowCorner[j] = 0;
-      inthighCorner[j] = N-1;
+      intPhaseXlowCorner[j] = 0;
+      intPhaseXhighCorner[j] = Np-1;
+      intPhaseVlowCorner[j] = -Np;
+      intPhaseVhighCorner[j] = Np-1;
     }
-  Point PhaselowCorner(intPhaselowCorner);
-  Point PhasehighCorner(intPhasehighCorner);
-  DBox PhaseSpace(PhaselowCorner, PhasehighCorner);
+  Point PhaseXlowCorner(intPhaseXlowCorner);
+  Point PhaseXhighCorner(intPhaseXhighCorner);
+  Point PhaseVlowCorner(intPhaseVlowCorner);
+  Point PhaseVhighCorner(intPhaseVhighCorner);
+  DBox PhaseXSpace(PhaseXlowCorner, PhaseXhighCorner);
+  DBox PhaseVSpace(PhaseVlowCorner, PhaseVhighCorner);
+  //PhaseXSpace.print();
+  //PhaseVSpace.print();
   //Still need to sort out what values this should be.
-  double k[DIM];
+  double Modes[DIM];
   for (int j=0; j<DIM; j++)
     {
-      k[j] = 1.0/2.0;
+      Modes[j] = 1.0/2.0;
     }
-  double alpha = 0.05;
   
+  cout<<"Num Particles = "<<Power(Np, DIM)*Power(2*Np, DIM) <<endl;
   if (test == 1)
   {
-    p.m_particles.resize(Power(N, 2));
+    p.m_particles.resize(Power(Np, DIM)*Power(2*Np, DIM));
     int j = 0;
-    for (Point pt=PhaseSpace.getLowCorner(); PhaseSpace.notDone(pt); PhaseSpace.increment(pt), j++)
+    for (Point ptV=PhaseVSpace.getLowCorner(); PhaseVSpace.notDone(ptV); PhaseVSpace.increment(ptV))
       {
-	for(int k=0; k<DIM; k++)
+	for (Point ptX=PhaseXSpace.getLowCorner(); PhaseXSpace.notDone(ptX); PhaseXSpace.increment(ptX), j++)
 	  {
-	    p.m_particles[j].m_x[k] = pt[k]*h;
-	    p.m_particles[j].m_v[k] = pt[DIM-1+k]*h;
-	    p.m_particles[j].EField[k] = 0.0;
+	    //p.m_particles.push_back();
+	    for(int k=0; k<DIM; k++)
+	      {
+		p.m_particles[j].m_x[k] = ptX[k]*hp*L;
+		p.m_particles[j].m_v[k] = ptV[k]*hp*vmax;
+		p.m_particles[j].EField[k] = 0.0;
+	      }
+	    double fFirstTerm= 1.0;
+	    double fSecondTerm =1.0;
+	    for(int k=0; k<DIM; k++)
+	      {
+		fFirstTerm *= exp(-p.m_particles[j].m_v[k]*p.m_particles[j].m_v[k]/2.0);
+		fSecondTerm *= exp(-p.m_particles[j].m_v[k]*p.m_particles[j].m_v[k]/2.0)*cos(Modes[k]*p.m_particles[j].m_x[k]);
+	      }
+	    p.m_particles[j].strength = 1/sqrt(2.0*M_PI)*(fFirstTerm + alpha*fSecondTerm)*pow(hp*L,DIM)*pow(hp*vmax,DIM);
 	  }
-	double fFirstTerm, fSecondTerm;
-	for(int k=0; k<DIM; k++)
-	  {
-	    fFirstTerm *= exp(-p.m_particles[j].m_v[k]*p.m_particles[j].m_v[k]/2.0);
-	    fSecondTerm *= exp(-p.m_particles[j].m_v[k]*p.m_particles[j].m_v[k]/2.0)*cos(k[dim]*p.m_particles[j].m_x[k]);
-	  }
-	p.m_particles[j].strength = 1/(2.0*M_PI)*(fFirstTerm + alpha*fSecondTerm)*pow(h,2*DIM);
       }
-    
   }
+
+  p.m_particles.erase(remove_if(p.m_particles.begin(), p.m_particles.end(), removeParticle), p.m_particles.end());
+
+  // for (auto it=p.m_particles.begin(); it!=p.m_particles.end(); ++it)
+  //   {
+  //     it->print();
+  //   }
+  
   //Need to check on some of these parameters.
   double dx = 1./N;
   cout << "number of particles = " << p.m_particles.size() << endl;
@@ -146,7 +178,7 @@ int main(int argc, char* argv[])
   kIn.setToZero();
   ParticleVelocities pv; 
   double time = 0.;
-  double dt = 140*.025/N;
+  double dt = 2.0/N;
   int m = 5000;
   
   RK4<ParticleSet,ParticleVelocities,ParticleShift> integrator;
